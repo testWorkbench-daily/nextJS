837fc726a4923ab020741c09025332bbce2b52de
杨林 <alice@aol.com>
2022-03-07 12:41:19 +0800

    new

---
+++ .next/static/gWtTIcAP8QRVYcr0F4ItV/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-e4f055287591f0f759f7.js"],"/_error":["static/chunks/pages/_error-eb7f79f2af83e8ddf5e2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
